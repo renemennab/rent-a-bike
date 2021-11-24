c0ec20921ff2bb33f2df259867cbe2f69c3448a8
Renê Boaventura Neto <reneboaventuraneto@gmail.com>
2021-11-24 15:48:55 -0300

    create bike controller

---
+++ server/controllers/bikeControllers.js
@@ -0,0 +1,25 @@
+import BikeModel from "../models/bikeModel.js"
+
+export async function getBikes(req, res) {
+    try {
+        const bikes = await BikeModel.find()
+        console.log(bikes)
+
+        return res.status(200).json(bikes)
+    } catch (error) {
+        res.status(404).json({ message: error.message })
+    }
+}
+
+export async function createBike(req, res) {
+    const bike = req.body
+
+    const newBike = new BikeModel(bike)
+    try {
+        await newBike.save()
+        // successful creation
+        res.status(201).json(newBike)
+    } catch (error) {
+        res.status(409).json({ message: error.message })
+    }
+}
