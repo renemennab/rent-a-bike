c5f9e726e106c999ed7d847eccead0d0965db061
Renê Boaventura Neto <reneboaventuraneto@gmail.com>
2021-11-25 11:46:56 -0300

    add more types

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,9 +1,30 @@
-export global {
-    interface IBike {
-        model: string
-        color: string
-        location: string
-        rating: number
-        isAvailable: boolean
-    }
+interface IBike extends PostBike {
+    id: string
 }
+
+interface PostBike {
+    model: string
+    color: string
+    location: string
+    rating: string
+    isAvailable: boolean
+}
+interface IUser extends PostUser {
+    id: string
+}
+
+interface PostUser extends LoginParams {
+    name: string
+}
+
+interface LoginParams {
+    email: string
+    password: string
+}
+interface LogInResponse {
+    name: string
+    id: string
+    email: string
+}
+
+type AssetType = 'user' | 'bike'
