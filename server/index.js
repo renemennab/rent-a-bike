75f48a559dabe3a7183d0312d0b06341e7dbfff7
Renê Boaventura Neto <reneboaventuraneto@gmail.com>
2021-11-25 18:58:59 -0300

    movo variable to .env

--- server/index.js
+++ server/index.js
@@ -2,20 +2,20 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
+import dotenv from 'dotenv'
 import bikeRoutes from './routes/bikeRouter.js'
 
 const app = express()
+dotenv.config()
+
 // @ts-ignore
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors())
 app.use('/bikes', bikeRoutes)
 
-const CONNECTION_URL =
-    'mongodb+srv://reneboaventuraneto:reneboaventuraneto123@cluster0.7jzx9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000
-
 mongoose
-    .connect(CONNECTION_URL)
+    .connect(process.env.CONNECTION_URL)
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch(err => console.log(err.message))
