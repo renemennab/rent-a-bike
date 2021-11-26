ce0f80b8393d8b5fb64ab9aebab39e6f216f5dfc
Renê Boaventura Neto <reneboaventuraneto@gmail.com>
2021-11-26 16:18:35 -0300

    add more types

--- src/types.d.ts
+++ src/types.d.ts
@@ -26,5 +26,33 @@ interface LogInResponse {
     id: string
     email: string
 }
+interface ILoginParams {
+    email: string
+    password: string
+}
+
+interface ISignupParams extends ILoginParams {
+    firstName: string
+    lastName: string
+}
+interface IUser {
+    firstName: string
+    lastName: string
+}
+
+interface IlocalStorageProfile {
+    result: IStorageResult
+    token: string
+}
+
+interface IStorageResult {
+    firstName: string
+    lastName: string
+    email: string
+    password: string
+    isManager: false
+    reservations: string[]
+    _id: string
+}
 
 type AssetType = 'user' | 'bike'
