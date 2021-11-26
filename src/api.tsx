6aeecd71922fef46ee850372948a552e40ea98ee
Renê Boaventura Neto <reneboaventuraneto@gmail.com>
2021-11-26 16:18:25 -0300

    add user apis

--- src/api.tsx
+++ src/api.tsx
@@ -2,16 +2,28 @@ import axios from 'axios'
 
 export const API_PATHS = {
     BIKES: 'bikes',
-    USERS: 'users',
-    LOGIN: 'login'
+    USER: 'user',
+    LOGIN: 'login',
+    SIGNUP: 'signup'
 }
 
-const baseUrl = 'https://bike-rental-manager.herokuapp.com/'
-interface IResponse {
+const isDev = process.env.NODE_ENV === 'development'
+const API = axios.create({ baseURL: isDev ? 'http://localhost:5000/' : 'https://bike-rental-manager.herokuapp.com/' })
+interface IBikeResponse {
     data: IBike
 }
-export const fetchBikes = (): Promise<IResponse> => axios.get(baseUrl + API_PATHS.BIKES)
-export const createBike = (newBike: PostBike): Promise<IResponse> => axios.post(baseUrl + API_PATHS.BIKES, newBike)
-export const updateBike = (bikeId: string, updatedbike: PostBike): Promise<IResponse> =>
-    axios.patch(`${baseUrl + API_PATHS.BIKES}/${bikeId}`, updatedbike)
-export const deleteBike = (bikeId: string): Promise<Response> => axios.delete(`${baseUrl + API_PATHS.BIKES}/${bikeId}`)
+
+export const fetchBikes = (): Promise<IBikeResponse> => API.get(API_PATHS.BIKES)
+export const createBike = (newBike: PostBike): Promise<IBikeResponse> => API.post(API_PATHS.BIKES, newBike)
+export const updateBike = (bikeId: string, updatedbike: PostBike): Promise<IBikeResponse> =>
+    API.patch(`${API_PATHS.BIKES}/${bikeId}`, updatedbike)
+export const deleteBike = (bikeId: string): Promise<Response> => API.delete(`${API_PATHS.BIKES}/${bikeId}`)
+
+interface IUserResponse {
+    data: IUser
+}
+
+export const createUser = (newUser: ISignupParams): Promise<IUserResponse> =>
+    API.post(`${API_PATHS.USER}/${API_PATHS.SIGNUP}`, newUser)
+export const loginUser = (user: ILoginParams): Promise<IUserResponse> =>
+    API.post(`${API_PATHS.USER}/${API_PATHS.LOGIN}`, user)
