80f8a61f1a8105589d80b64a394a081383cb2a48
Renê Boaventura Neto <reneboaventuraneto@gmail.com>
2021-11-26 16:21:58 -0300

    fix user profile

--- src/login/userProfile.tsx
+++ src/login/userProfile.tsx
@@ -1,15 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
 import PageHeader from '../common/pageHeader'
-import { SESSION_DATA } from './loginHelpers'
+import { getLoggedInUser } from './loginHelpers'
+
+interface IUserProfile {
+    name: string
+    email: string
+}
 
 const UserProfile = function (): JSX.Element {
+    const [userProfile, setUserProfile] = useState<IUserProfile>({ name: '', email: '' })
+
+    useEffect(() => {
+        const profile = getLoggedInUser()
+
+        if (profile) {
+            setUserProfile({
+                email: profile.result.email,
+                name: `${profile.result.firstName} ${profile.result.lastName}`
+            })
+        }
+    }, [])
+
     return (
         <StyledUserProfile className="userProfile">
-            <PageHeader pageName={window.sessionStorage.getItem(SESSION_DATA.NAME) || ``} />
+            <PageHeader pageName={userProfile.name || ``} />
             <span className="userProfile--email">
                 <strong>EMAIL: </strong>
-                {window.sessionStorage.getItem(SESSION_DATA.EMAIL)}
+                {userProfile.email}
             </span>
         </StyledUserProfile>
     )
