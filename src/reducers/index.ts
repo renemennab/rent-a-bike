5cc63a534a18a02eaf6a5961f9c96400df3b6732
Renê Boaventura Neto <reneboaventuraneto@gmail.com>
2021-11-26 20:18:22 -0300

    create selected user reducer

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux'
 import bikeReducer from './bikesReducer'
 import loggedUserReducer from './loggedUser'
 import selectedBikeReducer from './selectedBikeReducer'
+import selectedUserReducer from './selectedUserReducer'
 import usersReducer from './usersReducer'
 
 export default combineReducers({
     bikes: bikeReducer,
     selectedBike: selectedBikeReducer,
+    selectedUser: selectedUserReducer,
     loggedUser: loggedUserReducer,
     users: usersReducer
 })
