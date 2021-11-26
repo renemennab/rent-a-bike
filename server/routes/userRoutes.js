15e99130a27b540e920f96d8b9cd92b45f48d373
Renê Boaventura Neto <reneboaventuraneto@gmail.com>
2021-11-26 19:19:27 -0300

    create get server route for users

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express'
-import { login, signup } from '../controllers/userControllers.js'
+import { getUsers, login, signup } from '../controllers/userControllers.js'
 
 const userRouter = express.Router()
 
+userRouter.get('/', getUsers)
 userRouter.post('/login', login)
 userRouter.post('/signup', signup)
 
