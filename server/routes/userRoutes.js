30b53407f8bb3b5d72bacc769d8cfb2ec55f1e7b
Renê Boaventura Neto <reneboaventuraneto@gmail.com>
2021-11-26 20:16:18 -0300

    add more routes to user on server

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express'
-import { getUsers, login, signup } from '../controllers/userControllers.js'
+import { getUser, getUsers, login, signup, updateUser, deleteUser } from '../controllers/userControllers.js'
 
 const userRouter = express.Router()
 
 userRouter.get('/', getUsers)
 userRouter.post('/login', login)
 userRouter.post('/signup', signup)
-
+userRouter.get('/:id', getUser)
+userRouter.patch('/:id', updateUser)
+userRouter.delete('/:id', deleteUser)
 export default userRouter
