ebc5e62e915a78d5d878cf4b4f1c1e38b8c2ec5d
Renê Boaventura Neto <reneboaventuraneto@gmail.com>
2021-11-26 16:16:22 -0300

    add user routes to index

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import mongoose from 'mongoose'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import bikeRoutes from './routes/bikeRouter.js'
+import userRouter from './routes/userRoutes.js'
 
 const app = express()
 dotenv.config()
@@ -13,6 +14,7 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors())
 app.use('/bikes', bikeRoutes)
+app.use('/user', userRouter)
 app.get('/', (req, res) => res.send('Hello to RENT A BIKE API'))
 const PORT = process.env.PORT || 5000
 mongoose
