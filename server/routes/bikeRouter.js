0d8a2454d40bcc7e74d1a7cbc7e0c709277caa98
Renê Boaventura Neto <reneboaventuraneto@gmail.com>
2021-11-26 16:17:08 -0300

    use middleware in bike router

--- server/routes/bikeRouter.js
+++ server/routes/bikeRouter.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import { createBike, getBikes, updateBike, deleteBike } from '../controllers/bikeControllers.js'
+import middlewareAuth from '../middleware/middlewareAuth.js'
 
 const bikeRouter = express.Router()
 
 bikeRouter.get('/', getBikes)
-bikeRouter.post('/', createBike)
-bikeRouter.patch('/:id', updateBike)
-bikeRouter.delete('/:id', deleteBike)
+bikeRouter.post('/', middlewareAuth, createBike)
+bikeRouter.patch('/:id', middlewareAuth, updateBike)
+bikeRouter.delete('/:id', middlewareAuth, deleteBike)
 
 export default bikeRouter
