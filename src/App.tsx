54c30ded85f44ef41697d3718cb796bc7f67a087
Renê Boaventura Neto <reneboaventuraneto@gmail.com>
2021-11-27 19:49:40 -0300

    start reservations list

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import Login from './login/login'
 import UserProfileForm from './users/userProfileForm'
 import UsersList from './users/usersList'
 import SelectedUser from './users/selectedUser'
+import ReservationsList from './reservation/reservationsList'
 
 const App = function (): JSX.Element {
     const dispatch = useDispatch()
@@ -29,6 +30,7 @@ const App = function (): JSX.Element {
                         <Route path={`${ROUTES.BIKES}/:bikeId/edit`} component={BikeForm} />
                         <Route path={`${ROUTES.BIKES}/:bikeId`} component={SelectedBike} />
                         <Route path={ROUTES.BIKES} component={BikesList} />
+                        <Route path={ROUTES.RESERVATIONS} component={ReservationsList} />
                         <Route path={`${ROUTES.USERS}/:userId/edit`} component={UserProfileForm} />
                         <Route path={`${ROUTES.USERS}/:userId`} component={SelectedUser} />
                         <Route path={ROUTES.PROFILE} component={SelectedUser} />
---
+++ src/reservation/reservationsList.tsx
@@ -0,0 +1,87 @@
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useHistory } from 'react-router-dom'
+import styled from 'styled-components'
+import { getUserReservations } from '../actions/reservationActions'
+import { SELECTED_RESERVATION_REDUCER_OPTIONS } from '../reducers/selectedReservationReducer'
+
+import PageHeader from '../common/pageHeader'
+import { CardHeading, CardLink, CardSpan, ListCard } from '../common/listCard'
+import { FilterInput } from '../common/styled'
+import DateSelector from './dateSelector'
+import { getLoggedInUser } from '../login/loginHelpers'
+
+const ReservationsList = function (): JSX.Element {
+    const { reservations, reservationsByDates } = useSelector(
+        (state: { reservations: IReservation[]; reservationsByDates: IReservation[] }) => state
+    )
+    const reservationsData = reservationsByDates || reservations
+    const [filter, setFilter] = useState('')
+    const [filteredList, setFilteredList] = useState(reservationsData)
+    const history = useHistory()
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        if (!reservationsData.length) {
+            dispatch(getUserReservations(getLoggedInUser().result._id))
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
+    useEffect(() => {
+        const filtered = reservationsData?.reduce((acc: IReservation[], asset: IReservation) => {
+            const assetValues = Object.values(asset)
+            const matchingValues = assetValues.filter(value =>
+                value.toString().toLowerCase().includes(filter.toLowerCase())
+            )
+            if (matchingValues.length) acc.push(asset)
+
+            return acc
+        }, [])
+
+        setFilteredList(filtered)
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [filter, reservations, reservationsByDates])
+
+    const placeLink = history.location.pathname === '/' ? '' : history.location.pathname
+
+    return (
+        <StyledPlacesList>
+            <PageHeader pageName="reservations" />
+            <FilterInput
+                className="reservations--searchInput"
+                type="text"
+                value={filter}
+                onChange={event => setFilter(event.target.value)}
+            />
+            {filteredList.map((data: IReservation) => (
+                <ListCard className="reservations--card" key={data._id}>
+                    <CardLink
+                        className="reservations--card__link"
+                        to={`${placeLink}/${data._id}`}
+                        onClick={() =>
+                            dispatch({
+                                type: SELECTED_RESERVATION_REDUCER_OPTIONS.SET_SELECTED_RESERVATION,
+                                payload: data
+                            })
+                        }
+                    >
+                        <CardHeading className="reservations--card__link--model">{data.bikeId}</CardHeading>
+                        <CardSpan className="reservations--card__link--color">{data.startTimestamp}</CardSpan>
+                        <CardSpan className="reservations--card__link--location">{data.endTimestamp}</CardSpan>
+                    </CardLink>
+                </ListCard>
+            ))}
+        </StyledPlacesList>
+    )
+}
+
+export default ReservationsList
+
+const StyledPlacesList = styled.div`
+    padding: var(--padding);
+    flex-grow: 1;
+    width: 100%;
+    list-style: none;
+    margin: 0;
+`
