b4edbc1d3ad4545d35bed537dbc712c87c744e9d
Renê Boaventura Neto <reneboaventuraneto@gmail.com>
2021-11-30 11:15:04 -0300

    remove unused lib

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import 'leaflet/dist/leaflet.css'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
