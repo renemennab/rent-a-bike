1e61ef591be0254ab29eda3376c50e2662adfcbc
Renê Boaventura Neto <reneboaventuraneto@gmail.com>
2021-11-30 12:42:32 -0300

    lint login

--- src/login/login.tsx
+++ src/login/login.tsx
@@ -1,58 +1,65 @@
-import React, { FormEvent, useState } from 'react'
-import { useDispatch } from 'react-redux'
-import { Link, useHistory } from 'react-router-dom'
-import styled from 'styled-components'
-import { loginUser } from '../actions/userActions'
-import PageHeader from '../common/pageHeader'
-import { StyledButton, StyledForm } from '../common/styled'
-import UserInfo from '../common/userInfo'
-import { ROUTES } from '../utils'
+import React, { FormEvent, useState } from "react";
+import { useDispatch } from "react-redux";
+import { Link, useHistory } from "react-router-dom";
+import styled from "styled-components";
+import { loginUser } from "../actions/userActions";
+import PageHeader from "../common/pageHeader";
+import { StyledButton, StyledForm } from "../common/styled";
+import UserInfo from "../common/userInfo";
+import { ROUTES } from "../utils";
 
 const Login = function (): JSX.Element {
-    const [email, setEmail] = useState(``)
-    const [password, setPassword] = useState(``)
-    const [userNotFound, setUserNotFound] = useState(false)
-    const history = useHistory()
-    const dispatch = useDispatch()
-    function handleLogin(event: FormEvent<HTMLFormElement>): void {
-        event.preventDefault()
-        dispatch(loginUser({ email, password }, history, setUserNotFound))
-    }
+  const [email, setEmail] = useState(``);
+  const [password, setPassword] = useState(``);
+  const [userNotFound, setUserNotFound] = useState(false);
+  const history = useHistory();
+  const dispatch = useDispatch();
+  function handleLogin(event: FormEvent<HTMLFormElement>): void {
+    event.preventDefault();
+    dispatch(loginUser({ email, password }, history, setUserNotFound));
+  }
 
-    return (
-        <StyledLogin>
-            <PageHeader pageName="Login" />
-            <StyledForm action="" onSubmit={event => handleLogin(event)}>
-                <UserInfo email={email} setEmail={setEmail} password={password} setPassword={setPassword} />
-                <StyledButton>
-                    Login <i className="fas fa-sign-in-alt" />
-                </StyledButton>
-            </StyledForm>
+  return (
+    <StyledLogin>
+      <PageHeader pageName="Login" />
+      <StyledForm action="" onSubmit={(event) => handleLogin(event)}>
+        <UserInfo
+          email={email}
+          setEmail={setEmail}
+          password={password}
+          setPassword={setPassword}
+        />
+        <StyledButton>
+          Login <i className="fas fa-sign-in-alt" />
+        </StyledButton>
+      </StyledForm>
 
-            {userNotFound ? <span className="login--userNotFound">USER NOT FOUND</span> : null}
-            <Link to={ROUTES.SIGNUP} className="login--signupLink">
-                Need to create an account? Sign up here!
-            </Link>
-        </StyledLogin>
-    )
-}
+      {userNotFound ? (
+        <span className="login--userNotFound">USER NOT FOUND</span>
+      ) : null}
+      <Link to={ROUTES.SIGNUP} className="login--signupLink">
+        Need to create an account? Sign up here!
+      </Link>
+    </StyledLogin>
+  );
+};
 
-export default Login
+export default Login;
 const StyledLogin = styled.div`
-    padding: var(--padding);
-    display: flex;
-    flex-direction: column;
-    .login {
-        &--signupLink {
-            align-self: center;
-            text-decoration: none;
-            color: var(--dark-blue);
-            margin-top: 50px;
-        }
-        &--userNotFound {
-            color: var(--red);
-            margin-top: 40px;
-            align-self: center;
-        }
+  padding: var(--padding);
+  display: flex;
+  flex-direction: column;
+  .login {
+    &--signupLink {
+      align-self: center;
+      text-decoration: none;
+      color: var(--dark-blue);
+      margin-top: 50px;
     }
-`
+    &--userNotFound {
+      color: var(--red);
+      margin-top: 40px;
+      align-self: center;
+    }
+  }
+`;
--- src/login/loginHelpers.tsx
+++ src/login/loginHelpers.tsx
@@ -1,16 +1,16 @@
 export const SESSION_DATA = {
-    FIRST_NAME: 'firstName',
-    LAST_NAME: 'lastName',
-    EMAIL: 'email',
-    ID: '_id',
-    IS_MANAGER: 'isManager',
-    RESULT: 'result',
-    TOKEN: 'token',
-    PROFILE: 'profile'
-}
+  FIRST_NAME: "firstName",
+  LAST_NAME: "lastName",
+  EMAIL: "email",
+  ID: "_id",
+  IS_MANAGER: "isManager",
+  RESULT: "result",
+  TOKEN: "token",
+  PROFILE: "profile",
+};
 
 export function getLoggedInUser(): IlocalStorageProfile {
-    const profileString = window.localStorage.getItem(SESSION_DATA.PROFILE)
-    const profile = profileString ? JSON.parse(profileString) : null
-    return profile
+  const profileString = window.localStorage.getItem(SESSION_DATA.PROFILE);
+  const profile = profileString ? JSON.parse(profileString) : null;
+  return profile;
 }
