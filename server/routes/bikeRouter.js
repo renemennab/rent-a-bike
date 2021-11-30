54ac63b645c08587a5514d27c2b6673d57757190
Renê Boaventura Neto <reneboaventuraneto@gmail.com>
2021-11-30 12:41:10 -0300

    lint routes

--- server/routes/bikeRouter.js
+++ server/routes/bikeRouter.js
@@ -1,24 +1,27 @@
-import express from 'express'
-import pathConstants from '../../src/pathConstants.js'
+import express from "express";
+import pathConstants from "../../src/pathConstants.js";
 import {
-    createBike,
-    getBikes,
-    getBike,
-    updateBike,
-    deleteBike,
-    getBikesByDates,
-    rateBike
-} from '../controllers/bikeControllers.js'
-import middlewareAuth from '../middleware/middlewareAuth.js'
+  createBike,
+  getBikes,
+  getBike,
+  updateBike,
+  deleteBike,
+  getBikesByDates,
+  rateBike,
+} from "../controllers/bikeControllers.js";
+import middlewareAuth from "../middleware/middlewareAuth.js";
 
-const bikeRouter = express.Router()
+const bikeRouter = express.Router();
 
-bikeRouter.get('/:id', middlewareAuth, getBike)
-bikeRouter.get('/', middlewareAuth, getBikes)
-bikeRouter.get(`/${pathConstants.BIKES_BY_DATES}/:datesString`, getBikesByDates)
-bikeRouter.post('/', middlewareAuth, createBike)
-bikeRouter.patch('/:id', middlewareAuth, updateBike)
-bikeRouter.patch(`/:id/${pathConstants.RATING}`, middlewareAuth, rateBike)
-bikeRouter.delete('/:id', middlewareAuth, deleteBike)
+bikeRouter.get("/:id", middlewareAuth, getBike);
+bikeRouter.get("/", middlewareAuth, getBikes);
+bikeRouter.get(
+  `/${pathConstants.BIKES_BY_DATES}/:datesString`,
+  getBikesByDates
+);
+bikeRouter.post("/", middlewareAuth, createBike);
+bikeRouter.patch("/:id", middlewareAuth, updateBike);
+bikeRouter.patch(`/:id/${pathConstants.RATING}`, middlewareAuth, rateBike);
+bikeRouter.delete("/:id", middlewareAuth, deleteBike);
 
-export default bikeRouter
+export default bikeRouter;
