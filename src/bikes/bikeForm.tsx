7d5beecd1f1cab085aa43c3e33e8fb6804bab49b
Renê Boaventura Neto <reneboaventuraneto@gmail.com>
2021-11-30 12:42:00 -0300

    lint bikes

--- src/bikes/bikeForm.tsx
+++ src/bikes/bikeForm.tsx
@@ -1,93 +1,113 @@
-import React, { FormEvent, useEffect, useState } from 'react'
-import styled from 'styled-components'
-import { useHistory, useParams } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
-import { createBike, getBike, updateBike } from '../actions/bikeActions'
-import PageHeader from '../common/pageHeader'
-import { StyledButton, StyledForm, StyledInput, StyledLabel } from '../common/styled'
-import { ROUTES } from '../utils'
-import { SELECTED_BIKE_REDUCER_OPTIONS } from '../reducers/selectedBikeReducer'
+import React, { FormEvent, useEffect, useState } from "react";
+import styled from "styled-components";
+import { useHistory, useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { createBike, getBike, updateBike } from "../actions/bikeActions";
+import PageHeader from "../common/pageHeader";
+import {
+  StyledButton,
+  StyledForm,
+  StyledInput,
+  StyledLabel,
+} from "../common/styled";
+import { ROUTES } from "../utils";
+import { SELECTED_BIKE_REDUCER_OPTIONS } from "../reducers/selectedBikeReducer";
 
 const BikeForm = function (): JSX.Element {
-    const dispatch = useDispatch()
-    const { selectedBike } = useSelector((state: { selectedBike?: IBike }) => state)
+  const dispatch = useDispatch();
+  const { selectedBike } = useSelector(
+    (state: { selectedBike?: IBike }) => state
+  );
 
-    const [model, setModel] = useState(selectedBike?.model || ``)
-    const [color, setColor] = useState(selectedBike?.color || ``)
-    const [location, setLocation] = useState(selectedBike?.location || ``)
+  const [model, setModel] = useState(selectedBike?.model || ``);
+  const [color, setColor] = useState(selectedBike?.color || ``);
+  const [location, setLocation] = useState(selectedBike?.location || ``);
 
-    const history = useHistory()
-    const params = useParams() as { bikeId: string }
+  const history = useHistory();
+  const params = useParams() as { bikeId: string };
 
-    // const [relatedEvents, setRelatedEvents] = useState(``)
-    useEffect(() => {
-        if (!selectedBike && params.bikeId) {
-            dispatch(getBike(params.bikeId))
-        }
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
-    useEffect(() => {
-        if (selectedBike) {
-            setModel(selectedBike.model)
-            setColor(selectedBike.color)
-            setLocation(selectedBike.location)
-        }
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [selectedBike])
-
-    function handleBikeSubmit(event: FormEvent<HTMLFormElement>): void {
-        event.preventDefault()
-        const bikeData: PostBike = { isAvailable: true, color, location, model }
-        if (params.bikeId && selectedBike) {
-            dispatch(updateBike(selectedBike._id, bikeData))
-        } else {
-            dispatch(createBike(bikeData))
-        }
-        dispatch({ type: SELECTED_BIKE_REDUCER_OPTIONS.SET_SELECTED_BIKE, payload: null })
-        history.push(ROUTES.BIKES)
+  // const [relatedEvents, setRelatedEvents] = useState(``)
+  useEffect(() => {
+    if (!selectedBike && params.bikeId) {
+      dispatch(getBike(params.bikeId));
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  useEffect(() => {
+    if (selectedBike) {
+      setModel(selectedBike.model);
+      setColor(selectedBike.color);
+      setLocation(selectedBike.location);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [selectedBike]);
 
-    return (
-        <StyledNewColectionBike className="newBike">
-            <PageHeader pageName={params.bikeId ? 'Edit Bike' : 'New Bike'} />
-            <StyledForm action="" onSubmit={event => handleBikeSubmit(event)}>
-                <StyledLabel className="column">
-                    Model
-                    <StyledInput required type="text" value={model} onChange={event => setModel(event.target.value)} />
-                </StyledLabel>
-                <StyledLabel className="column">
-                    color
-                    <StyledInput required type="text" value={color} onChange={event => setColor(event.target.value)} />
-                </StyledLabel>
-                <StyledLabel className="column">
-                    location
-                    <StyledInput
-                        required
-                        type="text"
-                        value={location}
-                        onChange={event => setLocation(event.target.value)}
-                    />
-                </StyledLabel>
+  function handleBikeSubmit(event: FormEvent<HTMLFormElement>): void {
+    event.preventDefault();
+    const bikeData: PostBike = { isAvailable: true, color, location, model };
+    if (params.bikeId && selectedBike) {
+      dispatch(updateBike(selectedBike._id, bikeData));
+    } else {
+      dispatch(createBike(bikeData));
+    }
+    dispatch({
+      type: SELECTED_BIKE_REDUCER_OPTIONS.SET_SELECTED_BIKE,
+      payload: null,
+    });
+    history.push(ROUTES.BIKES);
+  }
 
-                <StyledButton>
-                    {selectedBike ? 'Update' : 'Save'} <i className="fa fa-save" />
-                </StyledButton>
-            </StyledForm>
-        </StyledNewColectionBike>
-    )
-}
+  return (
+    <StyledNewColectionBike className="newBike">
+      <PageHeader pageName={params.bikeId ? "Edit Bike" : "New Bike"} />
+      <StyledForm action="" onSubmit={(event) => handleBikeSubmit(event)}>
+        <StyledLabel className="column">
+          Model
+          <StyledInput
+            required
+            type="text"
+            value={model}
+            onChange={(event) => setModel(event.target.value)}
+          />
+        </StyledLabel>
+        <StyledLabel className="column">
+          color
+          <StyledInput
+            required
+            type="text"
+            value={color}
+            onChange={(event) => setColor(event.target.value)}
+          />
+        </StyledLabel>
+        <StyledLabel className="column">
+          location
+          <StyledInput
+            required
+            type="text"
+            value={location}
+            onChange={(event) => setLocation(event.target.value)}
+          />
+        </StyledLabel>
 
-export default BikeForm
+        <StyledButton>
+          {selectedBike ? "Update" : "Save"} <i className="fa fa-save" />
+        </StyledButton>
+      </StyledForm>
+    </StyledNewColectionBike>
+  );
+};
+
+export default BikeForm;
 
 const StyledNewColectionBike = styled.div`
-    padding: var(--padding);
-    fieldset {
-        border: none;
-        width: 100%;
-        .userType {
-            &--input {
-                margin-right: 10px;
-            }
-        }
+  padding: var(--padding);
+  fieldset {
+    border: none;
+    width: 100%;
+    .userType {
+      &--input {
+        margin-right: 10px;
+      }
     }
-`
+  }
+`;
--- src/bikes/bikesList.tsx
+++ src/bikes/bikesList.tsx
@@ -1,160 +1,193 @@
-import React, { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { Link, useHistory } from 'react-router-dom'
-import styled from 'styled-components'
-import { getBikes, setBikeRatingFilter } from '../actions/bikeActions'
-import { SELECTED_BIKE_REDUCER_OPTIONS } from '../reducers/selectedBikeReducer'
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useHistory } from "react-router-dom";
+import styled from "styled-components";
+import { getBikes, setBikeRatingFilter } from "../actions/bikeActions";
+import { SELECTED_BIKE_REDUCER_OPTIONS } from "../reducers/selectedBikeReducer";
 
-import PageHeader from '../common/pageHeader'
-import { CardAvailability, CardHeading, CardLink, CardRating, CardSpan, ListCard } from '../common/listCard'
-import { FilterInput } from '../common/styled'
-import DateSelector from '../reservation/dateSelector'
-import { RATING_OPTIONS, SEARCH_FILTERS_REDUCER_OPTIONS } from '../reducers/searchFiltersReducer'
-import { checkIfFilterMatchesBike } from '../common/utils'
-import { ROUTES } from '../utils'
-import { getLoggedInUser } from '../login/loginHelpers'
+import PageHeader from "../common/pageHeader";
+import {
+  CardAvailability,
+  CardHeading,
+  CardLink,
+  CardRating,
+  CardSpan,
+  ListCard,
+} from "../common/listCard";
+import { FilterInput } from "../common/styled";
+import DateSelector from "../reservation/dateSelector";
+import {
+  RATING_OPTIONS,
+  SEARCH_FILTERS_REDUCER_OPTIONS,
+} from "../reducers/searchFiltersReducer";
+import { checkIfFilterMatchesBike } from "../common/utils";
+import { ROUTES } from "../utils";
+import { getLoggedInUser } from "../login/loginHelpers";
 
 const BikesList = function (): JSX.Element {
-    const { bikes, bikesByDates } = useSelector((state: { bikes: IBike[]; bikesByDates: IBike[] }) => state)
-    const { bikeRating } = useSelector((state: { searchFilters: ISearchFilters }) => state.searchFilters)
+  const { bikes, bikesByDates } = useSelector(
+    (state: { bikes: IBike[]; bikesByDates: IBike[] }) => state
+  );
+  const { bikeRating } = useSelector(
+    (state: { searchFilters: ISearchFilters }) => state.searchFilters
+  );
 
-    const { isManager } = getLoggedInUser().result
-    const bikesData = isManager ? bikesByDates || bikes : bikesByDates
-    const [filter, setFilter] = useState('')
-    const [filteredList, setFilteredList] = useState(bikesData)
-    const history = useHistory()
-    const dispatch = useDispatch()
+  const { isManager } = getLoggedInUser().result;
+  const bikesData = isManager ? bikesByDates || bikes : bikesByDates;
+  const [filter, setFilter] = useState("");
+  const [filteredList, setFilteredList] = useState(bikesData);
+  const history = useHistory();
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        if (isManager) {
-            dispatch(getBikes())
-        }
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
-
-    useEffect(() => {
-        const filtered = bikesData?.reduce(
-            (acc: IBike[], bike: IBike) => (checkIfFilterMatchesBike(bike, filter) ? [...acc, bike] : acc),
-            []
-        )
-
-        setFilteredList(filtered)
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [filter, bikes, bikesByDates, bikeRating])
-
-    const placeLink = history.location.pathname === '/' ? '' : history.location.pathname
-
-    useEffect(() => {
-        const { BIKE_RATING } = SEARCH_FILTERS_REDUCER_OPTIONS
-
-        const sessionValue = window.sessionStorage.getItem(BIKE_RATING) || '0'
-        if (sessionValue !== bikeRating?.toString()) {
-            dispatch(setBikeRatingFilter(Number(sessionValue)))
-        }
-    }, [bikeRating])
-
-    const handleRatingFilterClick = (value: string): void => {
-        dispatch(setBikeRatingFilter(Number(value)))
+  useEffect(() => {
+    if (isManager) {
+      dispatch(getBikes());
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-    return (
-        <StyledBikesList className="bikesList">
-            <PageHeader pageName="Bikes" />
-            {isManager ? (
-                <Link to={ROUTES.NEW_BIKE} className="bikesList--AddBikeBtn ">
-                    <i className="fas fa-plus" />
-                </Link>
-            ) : null}
-            <FilterInput
-                className="bikesList--searchInput"
-                type="text"
-                value={filter}
-                onChange={event => setFilter(event.target.value)}
+  useEffect(() => {
+    const filtered = bikesData?.reduce(
+      (acc: IBike[], bike: IBike) =>
+        checkIfFilterMatchesBike(bike, filter) ? [...acc, bike] : acc,
+      []
+    );
+
+    setFilteredList(filtered);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [filter, bikes, bikesByDates, bikeRating]);
+
+  const placeLink =
+    history.location.pathname === "/" ? "" : history.location.pathname;
+
+  useEffect(() => {
+    const { BIKE_RATING } = SEARCH_FILTERS_REDUCER_OPTIONS;
+
+    const sessionValue = window.sessionStorage.getItem(BIKE_RATING) || "0";
+    if (sessionValue !== bikeRating?.toString()) {
+      dispatch(setBikeRatingFilter(Number(sessionValue)));
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [bikeRating]);
+
+  const handleRatingFilterClick = (value: string): void => {
+    dispatch(setBikeRatingFilter(Number(value)));
+  };
+
+  return (
+    <StyledBikesList className="bikesList">
+      <PageHeader pageName="Bikes" />
+      {isManager ? (
+        <Link to={ROUTES.NEW_BIKE} className="bikesList--AddBikeBtn ">
+          <i className="fas fa-plus" />
+        </Link>
+      ) : null}
+      <FilterInput
+        className="bikesList--searchInput"
+        type="text"
+        value={filter}
+        onChange={(event) => setFilter(event.target.value)}
+      />
+      <div className="bikesList--ratingInputs">
+        {RATING_OPTIONS.map((option, index) => (
+          <label
+            key={option}
+            htmlFor={`ratingFilterOption${index}`}
+            className="bikesList--ratingInputs__label"
+          >
+            <input
+              type="radio"
+              value={index}
+              checked={index === bikeRating}
+              name="rating"
+              id={`ratingFilterOption${index}`}
+              className="bikesList--ratingInputs__label--inputs"
+              onChange={(event) => handleRatingFilterClick(event.target.value)}
             />
-            <div className="bikesList--ratingInputs">
-                {RATING_OPTIONS.map((option, index) => {
-                    return (
-                        <label
-                            key={option}
-                            htmlFor={`ratingFilterOption${index}`}
-                            className="bikesList--ratingInputs__label"
-                        >
-                            <input
-                                type="radio"
-                                value={index}
-                                checked={index === bikeRating}
-                                name="rating"
-                                id={`ratingFilterOption${index}`}
-                                className="bikesList--ratingInputs__label--inputs"
-                                onChange={event => handleRatingFilterClick(event.target.value)}
-                            />
-                            {index ? `${index}+` : 'All'}
-                        </label>
-                    )
-                })}
-            </div>
-            <DateSelector />
-            {filteredList.map((data: IBike) => {
-                if (data.rateAverage < bikeRating) return null
-                return (
-                    <ListCard className={`bikesList--card ${data.isAvailable ? '' : 'unavailable'}`} key={data._id}>
-                        <CardLink
-                            className="bikesList--card__link"
-                            to={`${placeLink}/${data._id}`}
-                            onClick={() =>
-                                dispatch({ type: SELECTED_BIKE_REDUCER_OPTIONS.SET_SELECTED_BIKE, payload: data })
-                            }
-                        >
-                            <CardHeading className="bikesList--card__link--model">{data.model}</CardHeading>
-                            <CardSpan className="bikesList--card__link--color">{data.color}</CardSpan>
-                            <CardSpan className="bikesList--card__link--location">{data.location}</CardSpan>
-                            <CardRating
-                                className={`bikesList--card__link--rating rating${Math.floor(data.rateAverage)}`}
-                            >
-                                {data.rateAverage}
-                            </CardRating>
-                            <CardAvailability className={data.isAvailable ? '' : 'unavailable'}>
-                                {data.isAvailable ? (
-                                    <>
-                                        <i className="fas fa-check" /> Available
-                                    </>
-                                ) : (
-                                    <>
-                                        <i className="fas fa-times" /> Unavailable
-                                    </>
-                                )}
-                            </CardAvailability>
-                        </CardLink>
-                    </ListCard>
-                )
-            })}
-        </StyledBikesList>
-    )
-}
+            {index ? `${index}+` : "All"}
+          </label>
+        ))}
+      </div>
+      <DateSelector />
+      {filteredList.map((data: IBike) => {
+        if (data.rateAverage < bikeRating) return null;
+        return (
+          <ListCard
+            className={`bikesList--card ${
+              data.isAvailable ? "" : "unavailable"
+            }`}
+            key={data._id}
+          >
+            <CardLink
+              className="bikesList--card__link"
+              to={`${placeLink}/${data._id}`}
+              onClick={() =>
+                dispatch({
+                  type: SELECTED_BIKE_REDUCER_OPTIONS.SET_SELECTED_BIKE,
+                  payload: data,
+                })
+              }
+            >
+              <CardHeading className="bikesList--card__link--model">
+                {data.model}
+              </CardHeading>
+              <CardSpan className="bikesList--card__link--color">
+                {data.color}
+              </CardSpan>
+              <CardSpan className="bikesList--card__link--location">
+                {data.location}
+              </CardSpan>
+              <CardRating
+                className={`bikesList--card__link--rating rating${Math.floor(
+                  data.rateAverage
+                )}`}
+              >
+                {data.rateAverage}
+              </CardRating>
+              <CardAvailability
+                className={data.isAvailable ? "" : "unavailable"}
+              >
+                {data.isAvailable ? (
+                  <>
+                    <i className="fas fa-check" /> Available
+                  </>
+                ) : (
+                  <>
+                    <i className="fas fa-times" /> Unavailable
+                  </>
+                )}
+              </CardAvailability>
+            </CardLink>
+          </ListCard>
+        );
+      })}
+    </StyledBikesList>
+  );
+};
 
-export default BikesList
+export default BikesList;
 
 const StyledBikesList = styled.div`
-    padding: var(--padding);
-    flex-grow: 1;
-    width: 100%;
-    list-style: none;
-    margin: 0;
-    position: relative;
-    .bikesList {
-        &--ratingInputs {
-            margin: 10px 0 40px;
-            width: 100%;
-            max-width: 500px;
-            display: flex;
-            justify-content: space-around;
-        }
-        &--AddBikeBtn {
-            color: var(--yellow);
-            font-size: 35px;
-            position: absolute;
-            right: var(--padding);
-            top: var(--padding);
-        }
+  padding: var(--padding);
+  flex-grow: 1;
+  width: 100%;
+  list-style: none;
+  margin: 0;
+  position: relative;
+  .bikesList {
+    &--ratingInputs {
+      margin: 10px 0 40px;
+      width: 100%;
+      max-width: 500px;
+      display: flex;
+      justify-content: space-around;
     }
-`
+    &--AddBikeBtn {
+      color: var(--yellow);
+      font-size: 35px;
+      position: absolute;
+      right: var(--padding);
+      top: var(--padding);
+    }
+  }
+`;
