4cb09d62351a64d2f87f3caf8587d8315696aa71
Renê Boaventura Neto <reneboaventuraneto@gmail.com>
2021-11-30 12:42:22 -0300

    lint home

--- src/home/homePage.tsx
+++ src/home/homePage.tsx
@@ -1,90 +1,90 @@
-import React from 'react'
-import styled from 'styled-components'
-import { getLoggedInUser } from '../login/loginHelpers'
-import OptionsList from './optionsList'
-import Lamp from './lamp'
-import Logo from './logo'
-import TimeMoney from './timeMoney'
+import React from "react";
+import styled from "styled-components";
+import { getLoggedInUser } from "../login/loginHelpers";
+import OptionsList from "./optionsList";
+import Lamp from "./lamp";
+import Logo from "./logo";
+import TimeMoney from "./timeMoney";
 
 const HomePage = function (): JSX.Element {
-    const isUserLoggedIn = getLoggedInUser()
+  const isUserLoggedIn = getLoggedInUser();
 
-    return (
-        <StyledHomePage>
-            <div className="homePage--banner">
-                <div className="homePage--banner__background" />
-                <div className="homePage--banner__logo">
-                    <Logo />
-                </div>
-            </div>
-            {isUserLoggedIn ? null : (
-                <>
-                    <div className="homePage--energy">
-                        <Lamp className="homePage--energy__lamp" />
-                        <p className="homePage--energy__text">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        </p>
-                    </div>
-                    <div className="homePage--timeMoney">
-                        <p className="homePage--timeMoney__text">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        </p>
-                        <TimeMoney className="homePage--timeMoney__image" />
-                    </div>
-                </>
-            )}
+  return (
+    <StyledHomePage>
+      <div className="homePage--banner">
+        <div className="homePage--banner__background" />
+        <div className="homePage--banner__logo">
+          <Logo />
+        </div>
+      </div>
+      {isUserLoggedIn ? null : (
+        <>
+          <div className="homePage--energy">
+            <Lamp className="homePage--energy__lamp" />
+            <p className="homePage--energy__text">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </p>
+          </div>
+          <div className="homePage--timeMoney">
+            <p className="homePage--timeMoney__text">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </p>
+            <TimeMoney className="homePage--timeMoney__image" />
+          </div>
+        </>
+      )}
 
-            <OptionsList />
-        </StyledHomePage>
-    )
-}
+      <OptionsList />
+    </StyledHomePage>
+  );
+};
 
-export default HomePage
+export default HomePage;
 const StyledHomePage = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    padding-bottom: 40px;
-    .homePage {
-        &--menuBtn {
-            position: fixed;
-            left: 20px;
-            top: 20px;
-            background: var(--yellow);
-            border-radius: 8px;
-            padding: 15px;
-            color: white;
-            border: none;
-            font-size: 40px;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-        }
-        &--banner {
-            width: 100%;
-            height: 60vh;
-            min-height: 60vh;
-            background: var(--yellow);
-            display: flex;
-            justify-content: center;
-            align-items: center;
-        }
-        &--energy,
-        &--timeMoney {
-            width: 90%;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            margin-top: 20px;
-            &__text {
-                color: var(--dark-gray);
-                width: 250px;
-            }
-        }
-        &--timeMoney {
-            &__text {
-                width: 200px;
-            }
-        }
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-bottom: 40px;
+  .homePage {
+    &--menuBtn {
+      position: fixed;
+      left: 20px;
+      top: 20px;
+      background: var(--yellow);
+      border-radius: 8px;
+      padding: 15px;
+      color: white;
+      border: none;
+      font-size: 40px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
     }
-`
+    &--banner {
+      width: 100%;
+      height: 60vh;
+      min-height: 60vh;
+      background: var(--yellow);
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+    &--energy,
+    &--timeMoney {
+      width: 90%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      margin-top: 20px;
+      &__text {
+        color: var(--dark-gray);
+        width: 250px;
+      }
+    }
+    &--timeMoney {
+      &__text {
+        width: 200px;
+      }
+    }
+  }
+`;
--- src/home/optionsList.tsx
+++ src/home/optionsList.tsx
@@ -1,176 +1,200 @@
-import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
-import styled from 'styled-components'
-import { useDispatch } from 'react-redux'
-import { getLoggedInUser } from '../login/loginHelpers'
-import ConfirmationDialog from '../common/confirmationDialog'
-import { checkIfTokenIsExpired, ROUTES } from '../utils'
-import { SELECTED_BIKE_REDUCER_OPTIONS } from '../reducers/selectedBikeReducer'
-import { LOGGED_USER_REDUCER_OPTIONS } from '../reducers/loggedUser'
-import { SELECTED_USER_REDUCER_OPTIONS } from '../reducers/selectedUserReducer'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { useDispatch } from "react-redux";
+import { getLoggedInUser } from "../login/loginHelpers";
+import ConfirmationDialog from "../common/confirmationDialog";
+import { checkIfTokenIsExpired, ROUTES } from "../utils";
+import { SELECTED_BIKE_REDUCER_OPTIONS } from "../reducers/selectedBikeReducer";
+import { LOGGED_USER_REDUCER_OPTIONS } from "../reducers/loggedUser";
+import { SELECTED_USER_REDUCER_OPTIONS } from "../reducers/selectedUserReducer";
 
 interface IProps {
-    isNav?: boolean
+  isNav?: boolean;
 }
 const OptionsList = function ({ isNav }: IProps): JSX.Element {
-    const [showDialog, setShowDialog] = useState(false)
-    const [userIsLogged, setUserIsLogged] = useState(false)
-    const [userIsManager, setUserIsManager] = useState(false)
-    const dispatch = useDispatch()
+  const [showDialog, setShowDialog] = useState(false);
+  const [userIsLogged, setUserIsLogged] = useState(false);
+  const [userIsManager, setUserIsManager] = useState(false);
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch({ type: SELECTED_BIKE_REDUCER_OPTIONS.SET_SELECTED_BIKE, payload: null })
-        dispatch({ type: SELECTED_USER_REDUCER_OPTIONS.SET_SELECTED_USER, payload: null })
-    }, [])
+  useEffect(() => {
+    dispatch({
+      type: SELECTED_BIKE_REDUCER_OPTIONS.SET_SELECTED_BIKE,
+      payload: null,
+    });
+    dispatch({
+      type: SELECTED_USER_REDUCER_OPTIONS.SET_SELECTED_USER,
+      payload: null,
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-    useEffect(() => {
-        const user = getLoggedInUser()
+  useEffect(() => {
+    const user = getLoggedInUser();
 
-        if (user) {
-            setUserIsLogged(true)
-            if (user.result.isManager) setUserIsManager(true)
-            if (checkIfTokenIsExpired(user.token)) {
-                handleLogOut()
-            }
-        }
-    }, [])
-
-    function handleLogOut(): void {
-        dispatch({ type: LOGGED_USER_REDUCER_OPTIONS.LOGOUT_USER })
-        setUserIsLogged?.(false)
-        setShowDialog(false)
+    if (user) {
+      setUserIsLogged(true);
+      if (user.result.isManager) setUserIsManager(true);
+      if (checkIfTokenIsExpired(user.token)) {
+        handleLogOut();
+      }
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-    return (
-        <>
-            <StyledOptionsList className={`optionsList ${isNav ? 'isNav' : ''}`}>
-                {userIsLogged ? (
-                    <>
-                        <li className="optionsList--item ">
-                            <Link to={ROUTES.PROFILE} className="optionsList--item__button ">
-                                <i className="fas fa-user" /> My Profile
-                            </Link>
-                        </li>
-                        <li className="optionsList--item ">
-                            <Link to={ROUTES.BIKES} className="optionsList--item__button ">
-                                <i className="fas fa-bicycle" />
-                                Bikes
-                            </Link>
-                        </li>
-                        {userIsManager ? (
-                            <>
-                                <li className="optionsList--item ">
-                                    <Link to={ROUTES.USERS} className="optionsList--item__button ">
-                                        <i className="fas fa-users" /> Users
-                                    </Link>
-                                </li>
-                                <li className="optionsList--item ">
-                                    <Link to={ROUTES.NEW_BIKE} className="optionsList--item__button ">
-                                        <i className="fas fa-plus" />
-                                        Add New Bike
-                                    </Link>
-                                </li>
-                                <li className="optionsList--item ">
-                                    <Link to={ROUTES.NEW_USER} className="optionsList--item__button ">
-                                        <i className="fas fa-plus" />
-                                        Add New User
-                                    </Link>
-                                </li>
-                            </>
-                        ) : (
-                            <li className="optionsList--item ">
-                                <Link to={ROUTES.RESERVATIONS} className="optionsList--item__button ">
-                                    <i className="far fa-calendar-alt" />
-                                    My Reservations
-                                </Link>
-                            </li>
-                        )}
-                        <li className="optionsList--item ">
-                            <button
-                                type="button"
-                                onClick={() => setShowDialog(true)}
-                                className="optionsList--item__button "
-                            >
-                                <i className="fas fa-sign-out-alt" />
-                                Logout
-                            </button>
-                        </li>
-                    </>
-                ) : (
-                    <li className="optionsList--item ">
-                        <Link to={ROUTES.LOGIN} className="optionsList--item__button ">
-                            <i className="fas fa-sign-in-alt" />
-                            Login
-                        </Link>
-                    </li>
-                )}
-            </StyledOptionsList>
-            {showDialog ? (
-                <ConfirmationDialog onCancel={() => setShowDialog(false)} onDelete={() => handleLogOut()} text="sair" />
-            ) : null}
-        </>
-    )
-}
+  function handleLogOut(): void {
+    dispatch({ type: LOGGED_USER_REDUCER_OPTIONS.LOGOUT_USER });
+    setUserIsLogged?.(false);
+    setShowDialog(false);
+  }
 
-export default OptionsList
+  return (
+    <>
+      <StyledOptionsList className={`optionsList ${isNav ? "isNav" : ""}`}>
+        {userIsLogged ? (
+          <>
+            <li className="optionsList--item ">
+              <Link to={ROUTES.PROFILE} className="optionsList--item__button ">
+                <i className="fas fa-user" /> My Profile
+              </Link>
+            </li>
+            <li className="optionsList--item ">
+              <Link to={ROUTES.BIKES} className="optionsList--item__button ">
+                <i className="fas fa-bicycle" />
+                Bikes
+              </Link>
+            </li>
+            {userIsManager ? (
+              <>
+                <li className="optionsList--item ">
+                  <Link
+                    to={ROUTES.USERS}
+                    className="optionsList--item__button "
+                  >
+                    <i className="fas fa-users" /> Users
+                  </Link>
+                </li>
+                <li className="optionsList--item ">
+                  <Link
+                    to={ROUTES.NEW_BIKE}
+                    className="optionsList--item__button "
+                  >
+                    <i className="fas fa-plus" />
+                    Add New Bike
+                  </Link>
+                </li>
+                <li className="optionsList--item ">
+                  <Link
+                    to={ROUTES.NEW_USER}
+                    className="optionsList--item__button "
+                  >
+                    <i className="fas fa-plus" />
+                    Add New User
+                  </Link>
+                </li>
+              </>
+            ) : (
+              <li className="optionsList--item ">
+                <Link
+                  to={ROUTES.RESERVATIONS}
+                  className="optionsList--item__button "
+                >
+                  <i className="far fa-calendar-alt" />
+                  My Reservations
+                </Link>
+              </li>
+            )}
+            <li className="optionsList--item ">
+              <button
+                type="button"
+                onClick={() => setShowDialog(true)}
+                className="optionsList--item__button "
+              >
+                <i className="fas fa-sign-out-alt" />
+                Logout
+              </button>
+            </li>
+          </>
+        ) : (
+          <li className="optionsList--item ">
+            <Link to={ROUTES.LOGIN} className="optionsList--item__button ">
+              <i className="fas fa-sign-in-alt" />
+              Login
+            </Link>
+          </li>
+        )}
+      </StyledOptionsList>
+      {showDialog ? (
+        <ConfirmationDialog
+          onCancel={() => setShowDialog(false)}
+          onDelete={() => handleLogOut()}
+          text="sair"
+        />
+      ) : null}
+    </>
+  );
+};
+
+export default OptionsList;
 
 OptionsList.defaultProps = {
-    isNav: false
-}
+  isNav: false,
+};
 const StyledOptionsList = styled.ul`
-    padding: 0;
+  padding: 0;
+  .optionsList {
+    &--item {
+      list-style: none;
+      &__button {
+        width: 300px;
+        padding: 20px;
+        background: var(--yellow);
+        font-weight: 600;
+        border-radius: 8px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 20px;
+        margin-top: 20px;
+        &,
+        &:visited {
+          color: white;
+        }
+        i {
+          margin-right: 8px;
+        }
+        &:hover {
+          background: var(--red);
+        }
+      }
+    }
+  }
+  &.isNav {
     .optionsList {
-        &--item {
-            list-style: none;
-            &__button {
-                width: 300px;
-                padding: 20px;
-                background: var(--yellow);
-                font-weight: 600;
-                border-radius: 8px;
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                font-size: 20px;
-                margin-top: 20px;
-                &,
-                &:visited {
-                    color: white;
-                }
-                i {
-                    margin-right: 8px;
-                }
-                &:hover {
-                    background: var(--red);
-                }
-            }
-        }
-    }
-    &.isNav {
-        .optionsList {
-            &--item {
-                &__button {
-                    padding: 10px;
-                    border-radius: 10px;
-                    padding: 18px 0;
-                    border: none;
-                    background: white;
-                    text-decoration: none;
-                    font-size: 18px;
-                    justify-content: flex-start;
+      &--item {
+        &__button {
+          padding: 10px;
+          border-radius: 10px;
+          padding: 18px 0;
+          border: none;
+          background: white;
+          text-decoration: none;
+          font-size: 18px;
+          justify-content: flex-start;
 
-                    &,
-                    &:visited {
-                        color: var(--dark-blue);
-                    }
-                    i {
-                        color: var(--yellow);
-                    }
-                    &:hover {
-                        background: lightblue;
-                    }
-                }
-            }
+          &,
+          &:visited {
+            color: var(--dark-blue);
+          }
+          i {
+            color: var(--yellow);
+          }
+          &:hover {
+            background: lightblue;
+          }
         }
+      }
     }
-`
+  }
+`;
