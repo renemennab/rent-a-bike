b835731cd09b79a135eb42e69b26b60c4948cf39
Renê Boaventura Neto <reneboaventuraneto@gmail.com>
2021-12-01 14:53:15 -0300

    test bike reducer

---
+++ src/reducers/__tests__/testData.ts
@@ -0,0 +1,22 @@
+export function getBikeTestData(): IBike {
+  return {
+    model: "bike model",
+    color: "bike color",
+    location: "bike location",
+    _id: "bikeId",
+    rateAverage: 5,
+    userRatingValue: 5,
+    isAvailable: true,
+  };
+}
+export function getNewBikeTestData(): IBike {
+  return {
+    model: "new model",
+    color: "new color",
+    location: "new location",
+    _id: "newId",
+    rateAverage: 5,
+    userRatingValue: 5,
+    isAvailable: true,
+  };
+}
